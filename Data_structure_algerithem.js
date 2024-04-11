4c6715150ba75da23ea6645b5c0161aa698c8d3e
houssein sedoum <nejihoussein1@gmail.com>
2024-04-11 16:29:27 +0000

    leaning about linkedList

--- Data_structure_algerithem.js
+++ Data_structure_algerithem.js
@@ -1,21 +1,67 @@
 // queue
-const queue = [12, 33, 32]
-// linear data structure FIFO
+// const queue = [12, 33, 32]
+// // linear data structure FIFO
 
-queue.push(2)
-queue.shift(12)
-console.log(queue)
+// queue.push(2)
+// queue.shift(12)
+// console.log(queue)
 
 // stack
 //  linear  data structure follows LIFO
-const stack = []
+// const stack = []
 
-stack.push(1)
-stack.push(2)
-stack.push(3)
-stack.pop(3)
-console.log(stack)
-function peek() {
-  return stack[stack.length - 1]
+// stack.push(1)
+// stack.push(2)
+// stack.push(3)
+// stack.pop(3)
+// console.log(stack)
+// function peek() {
+//   return stack[stack.length - 1]
+// }
+// console.log("Peek", peek())
+
+// linked list
+
+function Node(data) {
+  return {
+    data: data,
+    next: null,
+  }
 }
-console.log("Peek", peek())
+
+function linkedList() {
+  let head = null
+
+  function append(data) {
+    let newNode = Node(data)
+
+    if (!head) {
+      head = newNode
+    } else {
+      let current = head
+      while (current.next) {
+        current = current.next
+      }
+      current.next = newNode
+    }
+  }
+
+  function print() {
+    let current = head
+    while (current) {
+      console.log(current.data)
+      current = current.next
+    }
+  }
+  return {
+    print,
+    append,
+  }
+}
+
+const link = linkedList()
+
+link.append(1)
+link.append(2)
+link.append(3)
+link.print()
